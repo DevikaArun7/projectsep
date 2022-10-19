f3b181d1f8fdaa28d8fb519fa2cdb046476eb39b
hp <devikaaruntv@gmail.com>
2022-10-19 11:13:52 +0530

    commit

---
+++ bin/projectsep.dart
@@ -0,0 +1,5 @@
+import 'package:projectsep/projectsep.dart' as projectsep;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projectsep.calculate()}!');
+}
---
+++ lib/Sample.dart
@@ -0,0 +1,8 @@
+void main(){
+  String name="Devika";
+  int age=21;
+  print("My name is ${name} i'm ${age} yrs old");
+int a=10;
+int b=20;
+print("sum=${a+b}");
+}
