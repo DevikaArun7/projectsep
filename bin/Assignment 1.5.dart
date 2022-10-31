5af500bff71af9c368d33ba919a9a3ba30f97f14
hp <devikaaruntv@gmail.com>
2022-10-31 13:57:27 +0530

    commit

---
+++ bin/Assignment 1.5.dart
@@ -0,0 +1,8 @@
+void main(){
+  int num=5 ,i,sum=0;
+  for(i=1;i<=10;i++){
+    sum=num*i;
+    print('$sum');
+  }
+
+}
---
+++ bin/Assignmet 1.4.dart
@@ -0,0 +1,25 @@
+void odd(){
+  int i,sum=0;
+  for(i=1;i<=10;i++){
+    if(i%2!=0){
+
+      sum=sum+i;
+    }
+  }
+  print('$sum');
+}
+
+void even(){
+  int i,sum=0;
+  for(i=1;i<=10;i++){
+    if(i%2==0){
+
+      sum=sum+i;
+    }
+  }
+  print('$sum');
+}
+void main(){
+  even();
+  odd();
+}
