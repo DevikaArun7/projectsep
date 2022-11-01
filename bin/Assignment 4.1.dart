59f8d8004f219b9db8252c2322d160161abe4f0a
hp <devikaaruntv@gmail.com>
2022-11-01 15:51:27 +0530

    commit

---
+++ bin/Assignment 4.1.dart
@@ -0,0 +1,13 @@
+class MainClass{
+  var name="Manu";
+  MainClass(){
+    print('Hii im Manu');
+  }
+  void show(){
+    print('Im 22');
+  }
+}
+void main(){
+  MainClass obj = MainClass();
+  obj.show();
+}
