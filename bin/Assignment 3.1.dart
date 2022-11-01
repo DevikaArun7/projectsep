c6ff48c10a86c305bfe5ff42d0c3a06cd1711f85
hp <devikaaruntv@gmail.com>
2022-11-01 12:31:19 +0530

    commit

---
+++ bin/Assignment 3.1.dart
@@ -0,0 +1,12 @@
+void main(){
+  student('Manu', 'manu@gmail.com ', 'zzz', '20', '89425667893', '468293');
+}
+void student(var name,email,housename,age, phone, pincode) {
+  print('Name=$name');
+  print('Age=$age');
+  print('Email=$email');
+  print('Phone=$phone');
+  print('Housename=$housename');
+  print('Pincode=$pincode');
+
+}
